6ddf46af5b6330d6e02eb0f9b5add208c7045696
ShivanshAr97 <shivansh.arora973@gmail.com>
2024-06-19 01:02:12 +0530

    commit, deploy change

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const server = app.listen(process.env.PORT, () =>
 
 const io = new Server(server,{
   cors:{
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173", "https://convostream.onrender.com"],
     credentials: true,
   }
 })
